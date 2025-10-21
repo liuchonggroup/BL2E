5e1e54136ee84621843fd93e967a474facde346f
liuchonggroup <96228040+liuchonggroup@users.noreply.github.com>
2025-10-21 13:54:53 +0800

    Add files via upload

--- infer.py
+++ infer.py
@@ -2,36 +2,34 @@ import os.path as osp
 
 import numpy as np
 import onnxruntime as rt
+from sklearn.preprocessing import OneHotEncoder
 
-def run_onnx_inference(bl_length: float):
-    """
-    加载 ONNX 模型并用它来进行推理。
-    """
-    bl_length = np.array([[bl_length]], dtype=np.float32)
-    onnx_path = osp.join(osp.dirname(__file__), 'onnx', 'BL2Emodel.onnx')
-    print("\n--- 步骤 3: 使用 ONNX Runtime 进行推理 ---")
+# 创建一个推理会话 (InferenceSession)
+onnx_path = osp.join(osp.dirname(__file__), 'onnx', 'BL2Emodel.onnx')
+sess_options = rt.SessionOptions()
+# 使用 Intel Extension for Scikit-learn 优化后的模型需要注册自定义操作
+# sess_options.register_custom_ops_library("libskl2onnx_runtime.so")
+sess = rt.InferenceSession(onnx_path, sess_options=sess_options, providers=["CPUExecutionProvider"])
 
+onehot_encoder = OneHotEncoder(categories=[np.array(['Zr', 'Sc', 'Sr', 'Cu', 'Ga'])], sparse_output=False)
+def infer_model(bl: float, metal_sym: str):
+    if metal_sym not in ['Zr', 'Sc', 'Sr', 'Cu', 'Ga']:
+        raise ValueError(f'The infer model only supports [Zr, Sc, Sr, Cu, Ga], got {metal_sym}')
+    if not (0.7 < bl < 2.1):
+        raise ValueError(f'The bond length only allows ranges from 0.7 to 2.1, got {bl}')
 
-    # 创建一个推理会话 (InferenceSession)
-    sess_options = rt.SessionOptions()
-    # 使用 Intel Extension for Scikit-learn 优化后的模型需要注册自定义操作
-    # sess_options.register_custom_ops_library("libskl2onnx_runtime.so")
-    sess = rt.InferenceSession(onnx_path, sess_options=sess_options, providers=["CPUExecutionProvider"])
+    bl = np.array([[bl]], dtype=np.float32)
+    sym_vec = onehot_encoder.fit_transform([[metal_sym]])
 
-    # 获取模型的输入和输出名称
+    x = np.hstack((bl, sym_vec), dtype=np.float32)
+
     input_name = sess.get_inputs()[0].name
     output_name = sess.get_outputs()[0].name
-    print(f"ONNX 模型输入名: '{input_name}'")
-    print(f"ONNX 模型输出名: '{output_name}'")
 
-    # 使用 ONNX 模型进行预测
-    # 注意：输入必须是一个字典，键是输入名，值是 numpy 数组
-    onnx_predictions = sess.run([output_name], {input_name: bl_length})[0]
-
-    print(f"预测能量值为： {onnx_predictions.item()} eV")
-
-    return onnx_predictions.item()
+    results = sess.run([output_name], {input_name: x})[0]
+    print(f"预测能量值为： {results.item()} eV")
+    return results
 
 
 if __name__ == "__main__":
-    print(run_onnx_inference(1.0))
+    infer_model(0.75, 'Sr')
